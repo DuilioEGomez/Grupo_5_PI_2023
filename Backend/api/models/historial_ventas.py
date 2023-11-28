ff861324ca21266b9ce976323a467ebb73bc2fd2
Duilio <130569141+DuilioEGomez@users.noreply.github.com>
2023-11-28 15:26:02 -0300

    se mejoraron las consultas SQL para obtener mas ordenada la informacion a entregar al Frontend

--- Backend/api/models/historial_ventas.py
+++ Backend/api/models/historial_ventas.py
@@ -1,30 +1,27 @@
 class Historial():
     def __init__(self,row):
-        self._fecha_factura = row[0]
-        self._id_usuario = row[1]
-        # self._id = row[2]
-        # self._id_usuario = row[3]
-        self._nombre = row[4]
-        self._apellido = row[5]
-        self._cuit = row[6]
-        self._id_factura = row[7]
-        self._id_producto = row[8]
-        self._cantidad = row[9]
-        self._precio_producto = row[10]
-        self._nombre_producto = row[11]        
-        # self._id = row[12]
+        self._apellido = row[0]
+        self._nombre = row[1]
+        self._cuit = row[2]
+        self._cantidad = row[3]
+        self._precio_producto = row[4]
+        self._nombre_producto = row[5]        
+        self._fecha_factura = row[6]
+        self._id = row[7]
+        self._id_usuario = row[8]
     
     def to_json(self):
         return {
-            "fecha factura" : self._fecha_factura,
-            "id_usuario": self._id_usuario,
-            "nombre": self._nombre,
             "apellido" : self._apellido,
+            "nombre": self._nombre,
             "cuit" : self._cuit,
-            "id_factura" : self._id_factura,
-            "id_producto" : self._id_producto,
             "cantidad" : self._cantidad,
-            "precio_producto" : self._precio_producto
+            "precio_producto" : self._precio_producto,
+            "nombre_producto" : self._nombre_producto,
+            "fecha factura" : self._fecha_factura,
+            "id factura" : self._id ,
+            "id_usuario" : self._id_usuario
+            
         }
 
     
