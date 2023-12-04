d1471baa38962629b286d98059ace5fa3cdc9503
Duilio <130569141+DuilioEGomez@users.noreply.github.com>
2023-12-03 21:46:09 -0300

    Correcion del login.js

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,5 +1,3 @@
-//document.getElementById('loginForm').addEventListener('submit', function(event) {
-  //  event.preventDefault();
 document.getElementById('submitButton').addEventListener('click', function() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
